42c90ddd758081e5546c71e8aac03dbfb3cc622e
Bohdan Hrytsenko <b.i.gricenko@gmail.com>
2023-08-31 17:29:36 +0300

    chore: small refactoring in RpcVeiw module

--- lib/rpc_view.ex
+++ lib/rpc_view.ex
@@ -7,28 +7,28 @@ defmodule RpcView do
 
   ## Examples
 
-      iex> RpcView.strip_meta_info(%{__meta__: "test", id: 1})
+      iex> RpcView.render(%{__meta__: "test", id: 1})
       %{id: 1}
 
-      iex> RpcView.strip_meta_info(%{__meta__: "test", id: 1, comments: %Ecto.Association.NotLoaded{}})
+      iex> RpcView.render(%{__meta__: "test", id: 1, comments: %Ecto.Association.NotLoaded{}})
       %{id: 1}
   """
 
-  def strip_meta_info(data) when is_map(data) do
+  def render(data) when is_map(data) do
     data
     |> Map.drop([:__meta__, :__struct__])
     |> Enum.reduce(%{}, fn {key, value}, acc ->
-      unless assoc_not_loaded?(value) do
-        Map.put(acc, key, strip_meta_info(value))
+      if not assoc_not_loaded?(value) do
+        Map.put(acc, key, render(value))
       else
         acc
       end
     end)
   end
 
-  def strip_meta_info(data) when is_list(data), do: Enum.map(data, &strip_meta_info/1)
+  def render(data) when is_list(data), do: Enum.map(data, &render/1)
 
-  def strip_meta_info(data), do: data
+  def render(data), do: data
 
   defp assoc_not_loaded?(%Ecto.Association.NotLoaded{}), do: true
   defp assoc_not_loaded?(_), do: false
--- test/rpc_view_test.exs
+++ test/rpc_view_test.exs
@@ -2,17 +2,17 @@ defmodule RpcViewTest do
   use ExUnit.Case
   doctest RpcView
 
-  describe "strip_meta_info/1" do
+  describe "render/1" do
     test "returns map without __metadata__ key" do
       post = Schema.Comment.changeset(%{body: "test_body"})
 
-      assert RpcView.strip_meta_info(post) == %{body: "test_body", id: nil}
+      assert RpcView.render(post) == %{body: "test_body", id: nil}
     end
 
     test "returns map without __metadata__ key and NotLoaded associations" do
       post = Schema.Post.changeset(%{title: "test_title", body: "test_body"})
 
-      assert RpcView.strip_meta_info(post) == %{title: "test_title", body: "test_body", id: nil}
+      assert RpcView.render(post) == %{title: "test_title", body: "test_body", id: nil}
     end
 
     test "returns map without __metadata__ key and NotLoaded associations. Additionaly apply this fuction for internal maps and structs" do
@@ -21,7 +21,7 @@ defmodule RpcViewTest do
       comment = Ecto.build_assoc(post, :comments, %{body: "test_comment"})
       post_map = post |> Map.from_struct() |> Map.put(:comments, [comment])
 
-      assert RpcView.strip_meta_info(post_map) == %{
+      assert RpcView.render(post_map) == %{
                title: "test_title",
                body: "test_body",
                id: nil,
